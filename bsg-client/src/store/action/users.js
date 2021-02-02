6bd2a58708ddf947a7f88b75ae15ac9be8a520b3
ning1315 <59202849+ning1315@users.noreply.github.com>
2021-02-02 20:25:28 +0900

    set redux action relation user V2

--- bsg-client/src/store/action/users.js
+++ bsg-client/src/store/action/users.js
@@ -4,7 +4,12 @@ export const SIGNUP_USER = 'SIGNUP_USER';
 export const SIGNIN_USER = 'SIGNIN_USER';
 export const SIGNOUT_USER = 'SIGNOUT_USER';
 export const GETUSER_DATA = 'GETUSER_DATA';
-export const SECEDE_USER = 'SECEDE_USER'
+export const SECEDE_USER = 'SECEDE_USER';
+export const SET_ACCESS_TOKEN ='SET_ACCESS_TOKEN'
+
+export const IS_LOGIN = 'IN_LOGIN';
+export const NOW_LOGIN = 'NOW_LOGIN'
+export const NOW_LOGOUT = 'NOW_LOGOUT'
 
 
 export const signUpUser = (data) => {
@@ -27,9 +32,99 @@ export const signOutUser = () => {
   }
 }
 
+export const getUserData = (data) => {
+  return {
+    type : GETUSER_DATA,
+    data
+  }
+}
+
 export const secedeUser = (data) => {
   return {
     type : SECEDE_USER,
     data
+  }
+}
+
+export const isLogin = () => {
+  return {
+    type : IS_LOGIN
+  }
+}
+
+export const nowLogIn = () => {
+  return {
+    type : NOW_LOGIN
+  }
+}
+
+export const nowLogOut = () => {
+  return {
+    type : NOW_LOGOUT
+  }
+}
+
+export const setAccessToken = (data) => {
+  return {
+    type : SET_ACCESS_TOKEN,
+    data
+  }
+}
+
+export const signingUpUser = (userdata) => {
+  return (dispatch) => {
+    return axios.post('서버 URL',{
+      data : userdata
+    })
+    .then((res) => {
+      if(res.message === 'OK'){
+        dispatch(setAccessToken(res.data.accessToken))
+        dispatch(getUserData(res.data))  
+        dispatch(nowLogIn())
+      }
+    }).catch((err) => {
+      throw(err)
+    })
+  }
+}
+
+export const signingInUser = (userdata) => {
+  return (dispatch) => {
+    return axios.post('서버 URL', {
+      data : userdata
+    })
+    .then((res) => {
+      if(res.message === 'OK'){
+        dispatch(setAccessToken(res.data.accessToken))
+        dispatch(getUserData(res.data))
+        dispatch(nowLogIn())
+      }
+    }).catch((err) =>{
+      throw(err)
+    })
+  }
+}
+
+export const signingOutUser = (accessToken) => {
+  return (dispatch) => {
+    return axios.post('서버 URL', {
+      accessToken 
+    }).then((res) => {
+      dispatch(getUserData(null))
+      dispatch(setAccessToken(null))
+      dispatch(nowLogOut())
+    })
+  }
+}
+
+export const secedingUser = (data) => {
+  return (dispatch) => {
+    return axios.post('서버 URL', {
+      data
+    }).then((res) => {
+      dispatch(getUserData(null))
+      dispatch(setAccessToken(null))
+      dispatch(nowLogOut())
+    })
   }
 }
