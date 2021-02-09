7a4a26669c0257ac345eb7f0724600e3cb06e36b
wnstjr0317 <qkrwnstjr0317@gmail.com>
2021-02-09 17:52:11 +0900

    Nav

--- bsg-client/src/component/Nav.js
+++ bsg-client/src/component/Nav.js
@@ -1,6 +1,7 @@
 import React,{ useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from "react-router-dom";
+import { Link } from 'react-router-dom'
 
 
 
@@ -28,9 +29,15 @@ const Nav = ({openModal}) => {
     <div className="Navbar">
       <div className="NavLeft">
       <img onClick={clickLogo} className="logo" src="https://ifh.cc/g/zvCv8t.png"></img>
+      <a href = 'https://kr.leagueoflegends.com/ko-kr/'target = '_blank'>
+        <button className = 'riotBtn'>공식사이트</button>
+      </a>
       </div>
       <div className="NavRight">
       <button onClick={() => {clickToBoard()}} className="navBtn">커뮤니티</button>
+      <a href ='http://lol.inven.co.kr/' target = '_blank'>
+      <button className = 'lolBtn'>LOL Inven</button>
+      </a>
       {isLogin ? <button onClick={() => {clickToMyPage()}} className="loginBtn">마이페이지</button> : ''}
       {isLogin ? <button className="loginBtn">로그아웃</button> : <button onClick={openModal} className="loginBtn">로그인</button>}
       </div>
